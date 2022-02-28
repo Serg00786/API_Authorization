3df5559d0368d07fd7ba49baed9da9c666fb1e05
Sergey <s.alekseev@hevelsolar.com>
2022-02-28 15:51:29 +0300

    Initial

---
+++ API_Authorization/Identity/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace API_Authorization
+{
+    public class AppUser : IdentityUser
+    {
+        public string Displayname { get; set; }
+
+    }
+}
---
+++ API_Authorization/Identity/DataContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace API_Authorization.Identity
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+    }
+}
